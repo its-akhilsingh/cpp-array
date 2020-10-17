7d126f97fd2b72c363552ba13f0e9674666c960c
its-akhilsingh <72935888+its-akhilsingh@users.noreply.github.com>
2020-10-17 12:05:29 +0530

    Add files via upload

---
+++ arrayinsertion.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int insertelement(int arr[],int n,int value,int pos)
+{
+  for(int t=n-1;t>=pos;t--)
+  {
+    arr[t+1]=arr[t];
+  }
+  arr[pos]=value;
+  return(n+1);
+}
+void printarray(int arr[],int n)
+{
+  cout<<"the array is";
+  for(int q=0;q<n;q++)
+  cout<<arr[q]<<" ";
+}
+int deleteelement(int arr[],int n,int pos)
+{
+  for(int i=pos;i<n-1;i++)
+  {
+    arr[i]=arr[i+1];
+  }return(n-1);
+}
+int main()
+{
+  int n,pos,value,arr[100];
+  cout<<"enter the no of elements you want to insert in array"<<endl;
+  cin>>n;
+  cout<<"enter the values in your array";
+for(int i=0;i<n;i++)
+{
+  cin>>arr[i];
+}
+printarray(arr,n);
+cout<<"enter value and its position";
+cin>>value;
+cin>>pos;
+n=insertelement(arr,n,value,pos);
+printarray(arr,n); int posi;
+cout<<"enter position at which you want to delete element";
+cin>>posi;
+n=deleteelement(arr,n,posi);
+printarray(arr,n);
+}
